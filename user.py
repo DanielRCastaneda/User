3950496b32f66209d8569fc5f93d53d970a74e8f
Daniel Castaneda <daniel02castaneda@gmail.com>
2022-03-22 17:24:32 -0700

    python

--- user.py
+++ user.py
@@ -5,41 +5,36 @@ class User:
     
     def make_deposit(self, amount):
         self.bank_statement += amount
+        return self
 
     def make_withdrawl(self, amount):
         self.bank_statement -= amount
+        return self
     
     def display_user_balance(self):
         print(f"User {self.name} has a balance of: ${self.bank_statement}")
+        return self
 
     def transfer_money(self, amount, user):
         self.bank_statement -= amount
         user.bank_statement += amount
         self.display_user_balance()
         user.display_user_balance()
+        return self
 
 
 user1 = User('Daniel',2500)
 user2 = User('Liz', 500)
 user3 = User('Marcos',15000)
 
-user1.make_deposit(960)
-user1.make_deposit(500)
-user1.make_deposit(250)
-user1.make_withdrawl(300)
-user1.display_user_balance()
+user1.make_deposit(960).make_deposit(500).make_deposit(250).make_withdrawl(400).display_user_balance()
 
-user2.make_deposit(10)
-user2.make_deposit(400)
-user2.make_withdrawl(5)
-user2.make_withdrawl(80)
-user2.display_user_balance()
 
-user3.make_deposit(2500)
-user3.make_withdrawl(1500)
-user3.make_withdrawl(5000)
-user3.make_withdrawl(2500)
-user3.display_user_balance()
+user2.make_deposit(10).make_deposit(400).make_withdrawl(5).make_withdrawl(80).display_user_balance()
+
+
+user3.make_deposit(2500).make_withdrawl(1500).make_withdrawl(5000).make_withdrawl(2500).display_user_balance()
+
 
 user1.transfer_money(500,user3)
 
